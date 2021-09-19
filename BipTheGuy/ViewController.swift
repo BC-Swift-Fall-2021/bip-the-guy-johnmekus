b33ba42bc757de5790da94b72d37f461367dfebd
johnmekus <mekus@bc.edu>
2021-09-19 15:03:58 -0400

    4.5

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -38,25 +38,37 @@ class ViewController: UIViewController {
             print("ERROR: Could not real error from file sound0.")
         }
     }
-
-    @IBAction func punchButtonPressed(_ sender: UIButton)
+    
+    func showAlert(title: String, message: String)
     {
-        let originalImageView = imageView.frame
-        let imageWidthShrink: CGFloat = 20
-        let imageHeightShrinnk: CGFloat = 20
-        let smallerImageFrame = CGRect(x: imageView.frame.origin.x + imageWidthShrink,
-          y: imageView.frame.origin.y + imageHeightShrinnk,
-          width: imageView.frame.width - (imageWidthShrink * 2),
-          height: imageView.frame.height - (imageHeightShrinnk * 2))
-        imageView.frame = smallerImageFrame
-        playSound(name: "punchSound")
-        UIView.animate(withDuration: 0.25,
-                       delay: 0.0,
-                       usingSpringWithDamping: 0.2,
-                       initialSpringVelocity: 10.0,
-                       options: [],
-                       animations: {self.imageView.frame = originalImageView},
-                       completion: nil)
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    @IBAction func photoOrCameraPressed(_ sender: UIButton)
+    {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default)
+        { _ in
+            print("You Clicked Photo Library")
+        }
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default)
+        { _ in
+            print("You Clicked Camera")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(cancelAction)
+        
+        present( alertController, animated: true, completion: nil)
+        
     }
     
     
